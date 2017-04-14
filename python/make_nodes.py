3c77256cbde0c185af5e8b2493bf51f50b8d76d9
hannahtwiggsmith <htwiggsmith@gmail.com>
2017-04-14 19:32:54 -0400

    Add .gitignore

--- python/make_nodes.py
+++ python/make_nodes.py
@@ -27,13 +27,13 @@ def make_links(data, num_recipes, cuisine):
                 nodelist.append(ingredient)
                 for other_ingredient in recipe["ingredients"]:
                     if ingredient != other_ingredient:
-                        links.append((ingredient,other_ingredient))
+                        links.append((ingredient, other_ingredient))
 
     return (nodelist, links)
 
 
 def make_json(link_list, hist, path="../data/ingredients.json"):
-    ingredient_struct = {"nodes":[], "links":[]}
+    ingredient_struct = {"nodes": [], "links": []}
     biggest_link = link_list[-1][1]
 
     ingredients = []
@@ -44,11 +44,15 @@ def make_json(link_list, hist, path="../data/ingredients.json"):
     for group in link_list:
         if group[0][0] not in ingredients:
             ingredients.append(group[0][0])
-            ingredient_struct["nodes"].append({"id": group[0][0], "group": groups[group[0][0]], "appearances":hist[group[0][0]]})
-        val = (group[1]/biggest_link)*10
-        #print(val)
+            ingredient_struct["nodes"].append(
+                {"id": group[0][0],
+                "group": groups[group[0][0]],
+                "appearances": hist[group[0][0]]})
+        val = (group[1] / biggest_link) * 10
+        # print(val)
 
-        ingredient_struct["links"].append({"source": group[0][0], "target": group[0][1], "value": val})
+        ingredient_struct["links"].append(
+            {"source": group[0][0], "target": group[0][1], "value": val})
 
     pickle.dump(ingredient_struct["links"], open("links.pickle", "wb"))
 
@@ -70,8 +74,7 @@ if __name__ == '__main__':
 
     c = Counter(elem for elem in links)
     appearances = Counter(elem for elem in nodelist)
-    ingredient_hist = {ing[0]:ing[1] for ing in appearances.items()}
+    ingredient_hist = {ing[0]: ing[1] for ing in appearances.items()}
 
-
-    l = [(key,value) for key, value in c.items() if value > 100]
-    make_json(sorted(l,key=lambda x: x[1]), ingredient_hist)
+    l = [(key, value) for key, value in c.items() if value > 100]
+    make_json(sorted(l, key=lambda x: x[1]), ingredient_hist)
